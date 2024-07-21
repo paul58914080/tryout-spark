4bda8232326165aacb09100b26ac276390ce4e7b
Paul Williams <paul58914080@gmail.com>
2024-07-21 17:32:40 +0530

    feat: use reduce for adding numbers

---
+++ src/main/kotlin/edu/kotlin/spark/reduce/Reduce.kt
@@ -0,0 +1,16 @@
+package edu.kotlin.spark.reduce
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+
+class Reduce {
+    val configuration: SparkConf = SparkConf().setAppName("Reduce").setMaster("local[*]")
+    val sc: JavaSparkContext = JavaSparkContext(configuration)
+
+    fun addAllElements(inputData: List<Int>): Int {
+        return sc.use {
+            it.parallelize(inputData)
+                .reduce { a, b -> a + b }
+        }
+    }
+}
---
+++ src/test/kotlin/edu/kotlin/spark/reduce/ReduceTest.kt
@@ -0,0 +1,18 @@
+package edu.kotlin.spark.reduce
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ReduceTest {
+
+    @Test
+    fun `test reduce function`() {
+        // Given
+        val inputData = listOf(1, 2, 3, 4, 5)
+        val reduce = Reduce()
+        // When
+        val result = reduce.addAllElements(inputData)
+        // Then
+        assertThat(result).isEqualTo(15)
+    }
+}
