00d0650febe5fc15c9b5af07f203528ca1de394d
Paul Williams <paul58914080@gmail.com>
2024-07-21 11:21:06 +0530

    feat: initial commit

---
+++ src/main/kotlin/edu/kotlin/spark/Hello World.kt
@@ -0,0 +1,18 @@
+package edu.kotlin.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+
+
+fun main() {
+    val inputData = listOf(1, 2, 3, 4, 5)
+
+    val configuration = SparkConf().setAppName("Hello World").setMaster("local[*]")
+    val sc = JavaSparkContext(configuration)
+    sc.use {
+        it.parallelize(inputData)
+            .map { it * 2 }
+            .collect()
+            .forEach(::println)
+    }
+}
