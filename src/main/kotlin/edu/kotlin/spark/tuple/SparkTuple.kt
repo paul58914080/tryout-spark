e666458ce4069cad87c199593c86c63aa873efe7
Paul Williams <paul58914080@gmail.com>
2024-07-22 20:42:52 +0530

    feat: get the sqrt of a given number with Tuple

---
+++ src/main/kotlin/edu/kotlin/spark/tuple/SparkTuple.kt
@@ -0,0 +1,15 @@
+package edu.kotlin.spark.tuple
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import scala.Tuple2
+import kotlin.math.sqrt
+
+class SparkTuple {
+  val configuration: SparkConf = SparkConf().setAppName("SparkTuple").setMaster("local[*]")
+  val sc: JavaSparkContext = JavaSparkContext(configuration)
+
+  fun computeSqrt(inputData: List<Int>): List<Tuple2<Int, Double>> {
+    return sc.use { it.parallelize(inputData).map { Tuple2(it, sqrt(it.toDouble())) }.collect() }
+  }
+}
---
+++ src/test/kotlin/edu/kotlin/spark/tuple/SparkTupleTest.kt
@@ -0,0 +1,26 @@
+package edu.kotlin.spark.tuple
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import scala.Tuple2
+
+class SparkTupleTest {
+
+  @Test
+  fun `print number and its sqrt`() {
+    // Given
+    val inputData = listOf(1, 2, 3, 4, 5)
+    val sparkTuple = SparkTuple()
+    // When
+    val result: List<Tuple2<Int, Double>> = sparkTuple.computeSqrt(inputData)
+    // Then
+    Assertions.assertThat(result)
+      .containsExactly(
+        Tuple2(1, 1.0),
+        Tuple2(2, 1.4142135623730951),
+        Tuple2(3, 1.7320508075688772),
+        Tuple2(4, 2.0),
+        Tuple2(5, 2.23606797749979),
+      )
+  }
+}
