ae4516bdf7bf96e630594317da47bc8bc04af6f6
Paul Williams <paul58914080@gmail.com>
2024-07-23 08:20:36 +0530

    feat(reduceByKey): count the number of logs by their log type

---
+++ src/main/kotlin/edu/kotlin/spark/reduce/ReduceByKey.kt
@@ -0,0 +1,20 @@
+package edu.kotlin.spark.reduce
+
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
+import scala.Tuple2
+
+class ReduceByKey {
+  val configuration: SparkConf = SparkConf().setAppName("Reduce").setMaster("local[*]")
+  val sc: JavaSparkContext = JavaSparkContext(configuration)
+
+  fun countLogType(inputData: List<String>): List<Tuple2<String, Long>> {
+    return sc.use {
+      it
+        .parallelize(inputData)
+        .mapToPair { Tuple2(it.split(":")[0], 1L) }
+        .reduceByKey { a, b -> a + b }
+        .collect()
+    }
+  }
+}
---
+++ src/test/kotlin/edu/kotlin/spark/reduce/ReduceByKeyTest.kt
@@ -0,0 +1,25 @@
+package edu.kotlin.spark.reduce
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import scala.Tuple2
+
+class ReduceByKeyTest {
+  @Test
+  fun `use reduceByKey to count the number of logs by their log type`() {
+    // Given
+    val inputData =
+      listOf(
+        "WARN: first warning",
+        "ERROR: first error",
+        "WARN: second warning",
+        "ERROR: second error",
+        "WARN: third warning",
+      )
+    val reduceByKey = ReduceByKey()
+    // When
+    val result = reduceByKey.countLogType(inputData)
+    // Then
+    assertThat(result).containsAll(listOf(Tuple2("WARN", 3), Tuple2("ERROR", 2)))
+  }
+}
