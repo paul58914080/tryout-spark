f3f0aba1e27e573fb1a0827b5ea004deece59bbf
Paul Williams <paul58914080@gmail.com>
2024-08-01 15:23:56 +0530

    feat: compute score for each course sorted by score

--- src/main/kotlin/edu/sample/spark/core/coursemetrics/CourseMetrics.kt
+++ src/main/kotlin/edu/sample/spark/core/coursemetrics/CourseMetrics.kt
@@ -1,31 +1,76 @@
 package edu.sample.spark.core.coursemetrics
 
-import org.apache.spark.SparkConf
+import java.io.Serializable
 import org.apache.spark.api.java.JavaPairRDD
 import org.apache.spark.api.java.JavaSparkContext
 import scala.Tuple2
 
-class CourseMetrics {
+class CourseMetrics : Serializable {
 
-  val sc: JavaSparkContext
+  fun computeScoreForEachCourse(jsc: JavaSparkContext): JavaPairRDD<String, Long> =
+    getCourseWithViewPercentagePairRDD(jsc)
+      .mapValues { getScore(it) } // (courseId, score)
+      .reduceByKey(Long::plus) // (courseId, score)
+      .join(getTitles(jsc)) // (courseId, (score, title))
+      .mapToPair { Tuple2(it._2._1, it._2._2) } // (score, title)
+      .sortByKey() // (score, title)
+      .mapToPair { Tuple2(it._2, it._1) } // (title, score)
 
-  init {
-    val configuration: SparkConf = SparkConf().setAppName("CourseMetrics").setMaster("local[*]")
-    sc = JavaSparkContext(configuration)
-  }
-
-  fun getCourseChapterCount(): List<Tuple2<Long, Long>> {
-    sc.use {
-      val courseChapterCountPairRDD = getCourseChapterCountPairRDD(it)
-      return courseChapterCountPairRDD
-        .reduceByKey { a, b -> a + b } // (courseId, totalNumberOfChapters)
-        .collect()
-    }
-  }
-
-  private fun getCourseChapterCountPairRDD(jpc: JavaSparkContext): JavaPairRDD<Long, Long> =
-    jpc.textFile("src/main/resources/viewing_figures/chapters.csv").mapToPair { row ->
+  private fun getTitles(jsc: JavaSparkContext): JavaPairRDD<Long, String> =
+    jsc.textFile("src/main/resources/viewing_figures/titles.csv").mapToPair { row ->
       val cols = row.split(",")
-      Tuple2(cols[1].toLong(), 1L)
+      Tuple2(cols[0].toLong(), cols[1]) // (courseId, title)
+    }
+
+  private fun getCourseWithViewPercentagePairRDD(jsc: JavaSparkContext): JavaPairRDD<Long, Double> =
+    getCourseWithViewsAndTotalChapterPairRDD(jsc).mapValues { it._1.toDouble() / it._2 }
+
+  private fun getCourseWithViewsAndTotalChapterPairRDD(
+    jsc: JavaSparkContext
+  ): JavaPairRDD<Long, Tuple2<Long, Long>> =
+    getCourseViewCountPairRDD(jsc).join(getCourseTotalChapterCountPairRDD(jsc))
+
+  fun getCourseTotalChapterCountPairRDD(jsp: JavaSparkContext): JavaPairRDD<Long, Long> {
+    val courseChapterCountAs1PairRDD = getCourseChapterAs1PairRDD(jsp)
+    return courseChapterCountAs1PairRDD.reduceByKey { a, b ->
+      a + b
+    } // (courseId, totalNumberOfChapters)
+  }
+
+  fun getCourseViewCountPairRDD(jsc: JavaSparkContext): JavaPairRDD<Long, Long> {
+    val views = jsc.textFile("src/main/resources/viewing_figures/views-*.csv")
+    val chapterAndUserMapToPair =
+      views
+        .mapToPair { row ->
+          val cols = row.split(",")
+          Tuple2(cols[1].toLong(), cols[0].toLong()) // (chapterId, userId)
+        }
+        .distinct()
+    val chapters = jsc.textFile("src/main/resources/viewing_figures/chapters.csv")
+    val chapterAndCoursePairRDD =
+      chapters.mapToPair { row ->
+        val cols = row.split(",")
+        Tuple2(cols[0].toLong(), cols[1].toLong()) // (chapterId, courseId)
+      }
+
+    return chapterAndUserMapToPair
+      .join(chapterAndCoursePairRDD) // (chapterId, (userId, courseId))
+      .mapToPair { Tuple2(it._2, 1L) } // ((userId, courseId), 1)
+      .reduceByKey { a, b -> a + b } // ((userId, courseId), viewCount)
+      .mapToPair { Tuple2(it._1._2, it._2) } // (courseId, viewCount)
+  }
+
+  private fun getCourseChapterAs1PairRDD(jsp: JavaSparkContext): JavaPairRDD<Long, Long> =
+    jsp.textFile("src/main/resources/viewing_figures/chapters.csv").mapToPair { row ->
+      val cols = row.split(",")
+      Tuple2(cols[1].toLong(), 1L) // (courseId, 1)
+    }
+
+  private fun getScore(percentage: Double): Long =
+    when {
+      percentage > 0.9 -> 10L
+      percentage > 0.5 -> 4L
+      percentage > 0.25 -> 2L
+      else -> 0L
     }
 }
--- src/test/kotlin/edu/sample/spark/core/coursemetrics/CourseMetricsTest.kt
+++ src/test/kotlin/edu/sample/spark/core/coursemetrics/CourseMetricsTest.kt
@@ -1,18 +1,74 @@
 package edu.sample.spark.core.coursemetrics
 
+import org.apache.spark.SparkConf
+import org.apache.spark.api.java.JavaSparkContext
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import scala.Tuple2
 
 class CourseMetricsTest {
+  val configuration: SparkConf = SparkConf().setAppName("CourseMetricsTest").setMaster("local[*]")
+  val sc = JavaSparkContext(configuration)
+
   @Test
   fun `compute the number of chapters for a given course`() {
-    // Given
-    val courseMetrics = CourseMetrics()
-    // When
-    val courseAndChapterCount: List<Tuple2<Long, Long>> = courseMetrics.getCourseChapterCount()
-    // Then
-    assertThat(courseAndChapterCount)
-      .containsAnyOf(Tuple2(1L, 34L))
+    sc.use {
+      // Given
+      val courseMetrics = CourseMetrics()
+      // When
+      val courseAndChapterCount: List<Tuple2<Long, Long>> =
+        courseMetrics.getCourseTotalChapterCountPairRDD(it).collect() // (courseId, totalNumberOfChapters)
+      // Then
+      assertThat(courseAndChapterCount).containsAnyOf(Tuple2(1L, 34L))
+    }
+  }
+
+  @Test
+  fun `compute the number of views for a given course`() {
+    sc.use {
+      // Given
+      val courseMetrics = CourseMetrics()
+      // When
+      val courseAndViewCount: List<Tuple2<Long, Long>> =
+        courseMetrics.getCourseViewCountPairRDD(it).collect() // (courseId, viewCount)
+      // Then
+      assertThat(courseAndViewCount).hasSize(10412).containsAnyOf(Tuple2(1, 10))
+    }
+  }
+
+  @Test
+  fun `compute score for each course sorted by score`() {
+    sc.use {
+      // Given
+      val courseMetrics = CourseMetrics()
+      // When
+      val scoreForEachCourse: List<Tuple2<String, Long>> =
+        courseMetrics.computeScoreForEachCourse(it).collect() // (title, score)
+      // Then
+      assertThat(scoreForEachCourse)
+        .hasSize(18)
+        .containsSequence(
+          listOf(
+            Tuple2("HTML5", 716),
+            Tuple2("Java Fundamentals", 2044),
+            Tuple2("Wildfly 1", 3188),
+            Tuple2("Wildfly 2", 3350),
+            Tuple2("Spring Security Module 2", 3678),
+            Tuple2("Spring Security Module 3", 4004),
+            Tuple2("Java Build Tools", 4830),
+            Tuple2("Spring Boot", 5384),
+            Tuple2("Android 1", 5416),
+            Tuple2("Thymeleaf", 5460),
+            Tuple2("NoSQL", 5508),
+            Tuple2("Cloud Deployment", 5616),
+            Tuple2("Spring Framework Fundmentals", 5616),
+            Tuple2("Spring Security Module 1 ", 5856),
+            Tuple2("SpringMVC", 5856),
+            Tuple2("Spring Remoting and Webservices", 5940),
+            Tuple2("Microservice Deployment", 5940),
+            Tuple2("Spring Boot Microservices", 5964),
+          )
+        )
+    }
   }
 }
