11ec4cee3c366d41816ad1950a136a11ea891a20
Paul Williams <paul58914080@gmail.com>
2024-08-02 10:15:39 +0530

    refactor(core): update the test for map

--- src/test/kotlin/edu/sample/spark/core/map/MappingTest.kt
+++ src/test/kotlin/edu/sample/spark/core/map/MappingTest.kt
@@ -6,12 +6,12 @@ class MappingTest {
 
   @Test fun `use map to compute the sqrt of a number`() {
     // Given
-    val inputData = listOf(1, 2, 3, 4, 5)
+    val inputData = listOf(1, 2, 3, 4)
     val mapping = Mapping()
     // When
     val result = mapping.computeSqrt(inputData)
     // Then
-    assert(result == listOf(1.0, 1.4142135623730951, 1.7320508075688772, 2.0, 2.23606797749979))
+    assert(result == listOf(1.0, 1.4142135623730951, 1.7320508075688772, 2.0))
   }
 
   @Test fun `use map and reduce to determine the count of elements`() {
