f38dc4b7c589ff3b3671cfe05aaeff895439e686
Paul Williams <paul58914080@gmail.com>
2024-08-06 09:08:51 +0530

    refactor(sql): rename CountStudents to CountRecords

--- src/main/kotlin/edu/sample/spark/sql/_2_16_getting_started/CountRecords.kt
+++ src/main/kotlin/edu/sample/spark/sql/_2_16_getting_started/CountRecords.kt
@@ -2,7 +2,7 @@ package edu.sample.spark.sql._2_16_getting_started
 
 import org.apache.spark.sql.SparkSession
 
-class CountStudents {
+class CountRecords {
   fun countStudentRecords(): Long {
     val sparkSession =
       SparkSession.builder().appName("CountStudents").master("local[*]").getOrCreate()
--- src/test/kotlin/edu/sample/spark/sql/_2_16_getting_started/CountRecordsTest.kt
+++ src/test/kotlin/edu/sample/spark/sql/_2_16_getting_started/CountRecordsTest.kt
@@ -3,14 +3,14 @@ package edu.sample.spark.sql._2_16_getting_started
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-class CountStudentsTest {
+class CountRecordsTest {
 
   @Test
   fun `count the number of students records`() {
     // Given
-    val countStudents = CountStudents()
+    val countRecords = CountRecords()
     // When
-    val count: Long = countStudents.countStudentRecords()
+    val count: Long = countRecords.countStudentRecords()
     // Then
     assertThat(count).isEqualTo(20_80_223L)
   }
