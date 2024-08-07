478ff87d8ded39a1f921f8d277eaf4f90b50f8e3
Paul Williams <paul58914080@gmail.com>
2024-08-07 08:53:52 +0530

    refactor(sql): rename methods for filters

--- src/main/kotlin/edu/sample/spark/sql/_2_17_datasets/Filter.kt
+++ src/main/kotlin/edu/sample/spark/sql/_2_17_datasets/Filter.kt
@@ -3,43 +3,55 @@ package edu.sample.spark.sql._2_17_datasets
 import org.apache.spark.sql.SparkSession
 
 class Filter {
-  fun firstRecordSubject(sparkSession: SparkSession): String {
-    val students =
-      sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
-    return students.first().getString(2)
+
+  fun firstRecordSubject(): String {
+    val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+    return sparkSession.use {
+      val students =
+        sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
+      students.first().getString(2)
+    }
   }
 
-  fun firstRecordYear(sparkSession: SparkSession): Int {
-    val students =
-      sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
-    return students.first().getAs<String?>(3).toInt()
+  fun firstRecordYear(): Int {
+    val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+    return sparkSession.use {
+      val students =
+        sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
+      students.first().getAs<String?>(3).toInt()
+    }
   }
 
-  fun countRecordsForSubject(sparkSession: SparkSession, subject: String): Long {
-    val students =
-      sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
-    return students.filter("subject = '$subject'").count() // this is like a `WHERE` clause in SQL
+  fun countRecordsForSubjectWithSqlFilters(subject: String): Long {
+    val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+    return sparkSession.use {
+      val students =
+        sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
+      students.filter("subject = '$subject'").count() // this is like a `WHERE` clause in SQL
+    }
   }
 
-  fun countRecordsForSubjectAndYear(sparkSession: SparkSession, subject: String, year: Int): Long {
-    val students =
-      sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
-    return students
-      .filter { row ->
-        row.getAs<String>("subject") == subject && row.getAs<String>("year").toInt() == year
-      }
-      .count()
+  fun countRecordsForSubjectAndYearWithJavaFilters(subject: String, year: Int): Long {
+    val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+    return sparkSession.use {
+      val students =
+        sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
+      students
+        .filter { row ->
+          row.getAs<String>("subject") == subject && row.getAs<String>("year").toInt() == year
+        }
+        .count()
+    }
   }
 
-  fun countRecordsBySubjectAndYearWithColFilters(
-    sparkSession: SparkSession,
-    subject: String,
-    year: Int,
-  ): Long {
-    val students =
-      sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
-    val subjectCol = students.col("subject")
-    val yearCol = students.col("year")
-    return students.filter(subjectCol.like(subject).and(yearCol.eqNullSafe(year))).count()
+  fun countRecordsForSubjectAndYearWithColFilters(subject: String, year: Int): Long {
+    val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+    return sparkSession.use {
+      val students =
+        sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
+      val subjectCol = students.col("subject")
+      val yearCol = students.col("year")
+      students.filter(subjectCol.like(subject).and(yearCol.eqNullSafe(year))).count()
+    }
   }
 }
--- src/test/kotlin/edu/sample/spark/sql/_2_17_datasets/FilterTest.kt
+++ src/test/kotlin/edu/sample/spark/sql/_2_17_datasets/FilterTest.kt
@@ -6,49 +6,49 @@ import org.junit.jupiter.api.Test
 
 class FilterTest {
   val sparkSession = SparkSession.builder().appName("FilterTest").master("local[*]").getOrCreate()
+  val studentsDataset =
+    sparkSession.read().option("header", "true").csv("src/main/resources/exams/students.csv")
 
   @Test
   fun `the first record subject`() {
-    sparkSession.use {
-      // Given
-      val filter = Filter()
-      // When
-      val subject: String = filter.firstRecordSubject(sparkSession)
-      // Then
-      assertThat(subject).isEqualTo("Math")
-      // When
-      val year = filter.firstRecordYear(sparkSession)
-      // Then
-      assertThat(year).isEqualTo(2005)
-    }
-  }
-
-  @Test
-  fun `count the records for a particular subject`() {
     // Given
     val filter = Filter()
     // When
-    val count: Long = filter.countRecordsForSubject(sparkSession, "Modern Art")
+    val subject: String = filter.firstRecordSubject()
+    // Then
+    assertThat(subject).isEqualTo("Math")
+    // When
+    val year = filter.firstRecordYear()
+    // Then
+    assertThat(year).isEqualTo(2005)
+  }
+
+  @Test
+  fun `count the records for a particular subject with 'sql' like filters`() {
+    // Given
+    val filter = Filter()
+    // When
+    val count: Long = filter.countRecordsForSubjectWithSqlFilters("Modern Art")
     // Then
     assertThat(count).isEqualTo(149_980L)
   }
 
   @Test
-  fun `count the records for a particular subject and year`() {
+  fun `count the records for a particular subject and year with 'java' like filters`() {
     // Given
     val filter = Filter()
     // When
-    val count: Long = filter.countRecordsForSubjectAndYear(sparkSession, "Modern Art", 2005)
+    val count: Long = filter.countRecordsForSubjectAndYearWithJavaFilters("Modern Art", 2005)
     // Then
     assertThat(count).isEqualTo(15_764L)
   }
 
   @Test
-  fun `count the records for a particular subject and year with column filters`() {
+  fun `count the records for a particular subject and year with 'column' filters`() {
     // Given
     val filter = Filter()
     // When
-    val count: Long = filter.countRecordsBySubjectAndYearWithColFilters(sparkSession, "Modern Art", 2005)
+    val count: Long = filter.countRecordsForSubjectAndYearWithColFilters("Modern Art", 2005)
     // Then
     assertThat(count).isEqualTo(15_764L)
   }
